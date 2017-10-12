ef5b7015245e9a0545192018c690aec533ef3061
liufei <363333424@qq.com>
2017-10-12 14:00:00 +0800

    231

--- src/com/liufei/test/Test.java
+++ src/com/liufei/test/Test.java
@@ -7,6 +7,7 @@ public class Test {
 	 */
 	public static void main(String[] args) {
            System.out.println("≤‚ ‘11211111123311221221213222211");
+           System.out.println("231");
 	}
 
 }
