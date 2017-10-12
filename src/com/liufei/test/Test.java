6badfce9057a6546e42f838d3d7837ef0aff297d
liufei <363333424@qq.com>
2017-10-12 11:09:29 +0800

    2222

--- src/com/liufei/test/Test.java
+++ src/com/liufei/test/Test.java
@@ -6,7 +6,7 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-           System.out.println("≤‚ ‘1121111112331122213222211");
+           System.out.println("≤‚ ‘11211111123311221221213222211");
 	}
 
 }
